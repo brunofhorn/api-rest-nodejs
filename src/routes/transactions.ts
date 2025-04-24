3194adf6e3551278937b815dd6da0b3111e72be2
brunofhorn <b.fernandeshorn@gmail.com>
2025-04-24 01:49:14 -0300

    feat: create post transaction

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,16 +1,27 @@
 import { FastifyInstance } from "fastify";
+import { z } from "zod";
 import { knex } from "../database";
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get("/hello", async () => {
-    const transaction = await knex("transactions")
+  app.post("/", async (request, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(["credit", "debit"]),
+    });
+
+    const { title, amount, type } = createTransactionBodySchema.parse(
+      request.body,
+    );
+
+    await knex("transactions")
       .insert({
         id: crypto.randomUUID(),
-        title: "Transação de teste",
-        amount: 1000,
+        title,
+        amount: type === "credit" ? amount : amount * -1,
       })
       .returning("*");
 
-    return transaction;
+    return reply.status(201).send();
   });
 }
