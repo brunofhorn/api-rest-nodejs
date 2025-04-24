4cc3a032f9615f0066bd2dea1e6e4de93847015f
brunofhorn <b.fernandeshorn@gmail.com>
2025-04-24 01:09:31 -0300

    feat: add env and env config with zod

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import fastify from "fastify";
 import { knex } from "./database";
+import { env } from "./env";
 
 const app = fastify();
 
@@ -15,6 +16,6 @@ app.get("/hello", async () => {
   return transaction;
 });
 
-app.listen({ port: 3333 }).then(() => {
+app.listen({ port: env.PORT }).then(() => {
   console.log("HTTP Server Running!");
 });
